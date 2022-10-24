898a2614c180501de6beaf59a43792c7bdfe7bd7
Joker-V2 <nipinna0@gmail.com>
2022-10-24 13:38:50 +0530

    spes: Don't include miui camera for now

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/xiaomi/spes/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Camera
-$(call inherit-product-if-exists, vendor/miuicamera/config.mk)
+# $(call inherit-product-if-exists, vendor/miuicamera/config.mk)
 
 # Product Specifics
 PRODUCT_NAME := lineage_spes
